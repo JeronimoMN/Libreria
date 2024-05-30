dedc319c64faacb1cf357f950e209ccf46003703
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-30 17:59:40 -0500

    Nuevas rutas agregadas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CatalogoLibrosComponent } from './catalogo-libros/catalogo-libros.component';
+import { LastBooksComponent } from './last-books/last-books.component';
 
 export const routes: Routes = [
     {path: 'catalogo-libros', component: CatalogoLibrosComponent},
+    {path: 'last-books', component: LastBooksComponent},
 
     {path: '', redirectTo: 'catalogo-libros', pathMatch: 'full'}
 ];
