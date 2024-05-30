7e2eefedf18e5bae89d3cf50e2350fbde24b088d
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-30 17:59:06 -0500

    Carrusel de libros recien llegados.

--- src/app/last-books/last-books.component.ts
+++ src/app/last-books/last-books.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { NgbCarouselConfig, NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-last-books',
   standalone: true,
-  imports: [],
+  imports: [NgbModule, CommonModule],
+  providers:[NgbCarouselConfig],
   templateUrl: './last-books.component.html',
   styleUrl: './last-books.component.css'
 })
-export class LastBooksComponent {
+export class LastBooksComponent implements OnInit {
 
+  ngOnInit(): void {
+    
+  }
 }
