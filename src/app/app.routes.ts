564b845a7d1f450c89561aefe1e141de3ce8bd5b
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-05-31 11:43:18 -0500

    Bootstrap-icons integrada. Nuevas rutas y estilos CSS.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { CatalogoLibrosComponent } from './catalogo-libros/catalogo-libros.component';
 import { LastBooksComponent } from './last-books/last-books.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 export const routes: Routes = [
+    {path: 'main-page', component: MainPageComponent},
     {path: 'catalogo-libros', component: CatalogoLibrosComponent},
     {path: 'last-books', component: LastBooksComponent},
+    {path: 'nav-bar', component: NavBarComponent},
 
-    {path: '', redirectTo: 'catalogo-libros', pathMatch: 'full'}
+    {path: '', redirectTo: 'main-page', pathMatch: 'full'}
 ];
