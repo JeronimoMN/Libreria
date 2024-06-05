d8c67ad5cfd74f8dd179d670b3f892f30c6f7cb8
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-06-05 13:14:53 -0500

    Cuandro de dialogo o Model Pop-Up del login en el Navbar Agregado. Se instalo una libreria Material de Angular.

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LoginComponent, MatDialogModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
 
+  constructor(private dialogRef: MatDialog) { }
+
+  onSubmit() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.minWidth = '40%'; // Ancho mínimo
+    dialogConfig.maxWidth = '100%'; // Ancho máximo
+    dialogConfig.hasBackdrop = true;
+    this.dialogRef.open(LoginComponent, dialogConfig)
+  }
 }
