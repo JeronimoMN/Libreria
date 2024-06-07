05ddb7286f150ae69f39b6f284d9cebbae07da74
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-06-07 11:09:41 -0500

    Filtro para busqueda de libros. Se empezo la paginación de Libros.

--- src/app/catalogo-libros/catalogo-libros.component.ts
+++ src/app/catalogo-libros/catalogo-libros.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-catalogo-libros',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NavBarComponent],
   templateUrl: './catalogo-libros.component.html',
   styleUrl: './catalogo-libros.component.css'
 })
 export class CatalogoLibrosComponent {
+
+  filterForm = new FormGroup({
+    'titulo': new FormControl(''),
+    'autor': new FormControl(''),
+    'categoria': new FormControl('')
+  })
+
+  onSubmit(){
+    console.log(this.filterForm.value)
+  }
+
+
+  books = [
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+    { titulo: 'Libro 1', autor: 'Autor 1', categoria: 'Categoría 1' },
+    { titulo: 'Libro 2', autor: 'Autor 2', categoria: 'Categoría 2' },
+
+  ]
+
+
 
 }
