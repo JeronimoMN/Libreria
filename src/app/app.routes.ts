8ba6f3c6d3475b09dfb6dd77ae06aed4093e660a
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-06-17 09:41:41 -0500

    Nuevas rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,24 @@ import { CatalogoLibrosComponent } from './catalogo-libros/catalogo-libros.compo
 import { MainPageComponent } from './main-page/main-page.component';
 import { ErrorComponent } from './error/error.component';
 import { RegisterComponent } from './shared/register/register.component';
+import { MainPageAdminComponent } from './admin/main-page-admin/main-page-admin.component';
+import { LoginComponent } from './shared/login/login.component';
+import { BookInventoryComponent } from './admin/book-inventory/book-inventory.component';
+import { AccountsComponent } from './admin/accounts/accounts.component';
 
 export const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
     { path: 'main-page', component: MainPageComponent },
     { path: 'catalogo-libros', component: CatalogoLibrosComponent },
     { path: 'error', component: ErrorComponent },
-    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'main-page-admin', component: MainPageAdminComponent, 
+        children: [
+            { path: 'book-inventory', component: BookInventoryComponent },
+            { path: 'order-inventory', component: BookInventoryComponent },
+            { path: 'accounts', component: AccountsComponent }
+        ]
+    },
     { path: '', redirectTo: 'main-page', pathMatch: 'full' },
     { path: '**', redirectTo: 'error', pathMatch: 'full' }
 ];
