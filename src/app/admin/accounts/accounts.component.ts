2ce1906d28554a21006fa147bccb4c9c4c21a3e6
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-06-21 12:04:48 -0500

    Componente para la gestion de las cuentas en proceso

--- src/app/admin/accounts/accounts.component.ts
+++ src/app/admin/accounts/accounts.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent {
+
+  accountForm = new FormGroup({
+    'username': new FormControl('', [Validators.required])
+  })
+
+  searchAccount() {
+    console.log(this.accountForm.value)
+  }
 
 }
