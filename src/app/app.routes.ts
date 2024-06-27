612af588d5275042772c8e81e1565341cd9f8c38
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2024-06-26 21:45:28 -0500

    Nuevos enrutamientos; Se especifican las diferentes rutas que un Admin puede estar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,21 @@ import { CatalogoLibrosComponent } from './catalogo-libros/catalogo-libros.compo
 import { MainPageComponent } from './main-page/main-page.component';
 import { ErrorComponent } from './error/error.component';
 import { RegisterComponent } from './shared/register/register.component';
-import { MainPageAdminComponent } from './admin/main-page-admin/main-page-admin.component';
 import { LoginComponent } from './shared/login/login.component';
 import { BookInventoryComponent } from './admin/book-inventory/book-inventory.component';
 import { AccountsComponent } from './admin/accounts/accounts.component';
 import { OrderInventoryComponent } from './admin/order-inventory/order-inventory.component';
+import { BookComponent } from './book/book.component';
+import { MainAdminLayoutComponent } from './layouts/main-admin-layout/main-admin-layout.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'main-page', component: MainPageComponent },
     { path: 'catalogo-libros', component: CatalogoLibrosComponent },
+    { path: 'book', component: BookComponent },
     { path: 'error', component: ErrorComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'main-page-admin', component: MainPageAdminComponent, 
+    { path: 'page-admin', component: MainAdminLayoutComponent, 
         children: [
             { path: 'book-inventory', component: BookInventoryComponent },
             { path: 'order-inventory', component: OrderInventoryComponent },
